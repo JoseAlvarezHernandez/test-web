b340d8bc1f3a9358c9695c0e4d581a8d5401b74b
Jose Alvarez <alvarez_3993@hotmail.com>
2018-05-09 17:30:23 -0500

    feature(server): modified server ports

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ let express = require('express');
 let app = express();
 let routes = require('./routes');
 let bodyParser = require('body-parser');
-let port = process.env.PORT || 1340;
+let port = process.env.PORT || 1339;
 // Set express rules
 app.use(bodyParser.urlencoded({
   extended: false,
@@ -16,4 +16,4 @@ app.use(express.static(path.join(__dirname, 'app')));
 app.get('/', routes.index);
 
 // Start server
-app.listen(port, () => { });
+app.listen(port, () => console.log('Server started'));
