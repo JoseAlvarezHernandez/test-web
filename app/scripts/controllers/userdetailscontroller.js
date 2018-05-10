3cdcce83f7fd429d481aeef270fcf531df20ad8b
Barranco Reyes <frbo@gft.com>
2018-05-10 16:14:22 -0500

    feature(login,users):adding token and email in localStorage, create module userDetailsController

---
+++ app/scripts/controllers/userdetailscontroller.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name moneyWeb.controller:UserdetailscontrollerCtrl
+ * @description
+ * # UserdetailscontrollerCtrl
+ * Controller of the moneyWeb
+ */
+angular
+  .module('moneyWeb')
+  .controller('UserDetailsController', UserController);
+
+UserController.$inject = ['Resource'];
+
+function UserController(Resource){
+  var uc = this;
+      uc.users = [];
+
+  activate();
+
+  function activate() {
+    return getUsers()
+      .then(function(res){
+        console.log('activated');
+      });
+  }
+
+  function getUsers(){
+    return Resource.getUsers(localStorage.getItem('token'), localStorage.getItem('email'))
+      .then(function(res) {
+        console.log(res);
+        uc.users = res.data;
+        return uc.users;
+      });
+  }
+}
